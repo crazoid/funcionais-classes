c419d696cb21cd2d91f41d50ad436c7274e31773
Fabio Palhano <crazoid@gmail.com>
2019-08-16 20:51:00 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class App2 extends React.Component {
+  contador = 1
+  increment = () => {
+    this.contador = this.contador+1
+  }
+  render(){
+    return (
+      <h1>Contador (app2): {this.contador}</h1>
+    )
+  }
+}
+
+const App1 = () => {
+  let contador = 1
+  const increment = () => {
+    contador = contador + 1
+  }
+  return (
+    <h1>Contador (app1): {contador}</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
